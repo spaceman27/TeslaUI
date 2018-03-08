fbde2234c28577ee0e3f4711d7c3b57c7fe47be1
Dao Nguyen <spaceman27@gmail.com>
2018-03-08 10:43:50 -0500

    update dist

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -21,19 +21,19 @@ export class HomeComponent implements OnInit {
 
   constructor() {
     this.lstProject = [
-      new ProjectModel('Flight UI Packs', 'assets/centric.jpg', '/flight', ['https://www.pinterest.com/pin/729864683334583637/']),
-      new ProjectModel('I Love Veggie', 'assets/iloveveggie.jpg', '', ['https://www.pinterest.com/pin/456763587197539948/']),
-      new ProjectModel('Disney Life', 'assets/disneylife.jpg', '', ['https://www.pinterest.com/pin/483503709984500652/']),
-      new ProjectModel('Sign In 3 Packs', 'assets/signin.jpg', '',
+      new ProjectModel('Flight UI Packs', 'centric.jpg', '/flight', ['https://www.pinterest.com/pin/729864683334583637/']),
+      new ProjectModel('I Love Veggie', 'iloveveggie.jpg', '', ['https://www.pinterest.com/pin/456763587197539948/']),
+      new ProjectModel('Disney Life', 'disneylife.jpg', '', ['https://www.pinterest.com/pin/483503709984500652/']),
+      new ProjectModel('Sign In 3 Packs', 'signin.jpg', '',
                         ['https://www.pinterest.com/pin/568790627925499096/',
                         'https://www.pinterest.com/pin/419608890260629056/',
                         'https://www.pinterest.com/pin/444378688224896820/'
                       ]),
-      new ProjectModel('Flat Transparent UI Kit', 'assets/flat-transparent-ui.jpg', '', ['https://dcrazed.com/wp-content/uploads/2013/09/mboy-flat-transparent.jpg']),    
-      new ProjectModel('Social Network Profile', 'assets/social.jpg', '', ['https://dcrazed.com/wp-content/uploads/2016/07/Flat-Design-UI-Components.jpg']),
-      new ProjectModel('Cooking Manual', 'assets/cooking.jpg', '', ['https://www.pinterest.com/pin/133700682659582280/']),
-      new ProjectModel('Flat UI Blog', 'assets/flat-ui-blog.jpg', '', ['https://dcrazed.com/wp-content/uploads/2013/09/flat-ui-blog-large-1.jpg']),
-      new ProjectModel('Web Dashboard', 'assets/web-dashboard.jpg', '', ['http://www.uipixels.com/web-dashboard-design-psd/'])
+      new ProjectModel('Flat Transparent UI Kit', 'flat-transparent-ui.jpg', '', ['https://dcrazed.com/wp-content/uploads/2013/09/mboy-flat-transparent.jpg']),    
+      new ProjectModel('Social Network Profile', 'social.jpg', '', ['https://dcrazed.com/wp-content/uploads/2016/07/Flat-Design-UI-Components.jpg']),
+      new ProjectModel('Cooking Manual', 'cooking.jpg', '', ['https://www.pinterest.com/pin/133700682659582280/']),
+      new ProjectModel('Flat UI Blog', 'flat-ui-blog.jpg', '', ['https://dcrazed.com/wp-content/uploads/2013/09/flat-ui-blog-large-1.jpg']),
+      new ProjectModel('Web Dashboard', 'web-dashboard.jpg', '', ['http://www.uipixels.com/web-dashboard-design-psd/'])
     ];
 
   }
