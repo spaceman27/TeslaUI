9556d270d08a3d791e8cfada897f946ba86e6ba1
Dao Nguyen <spaceman27@gmail.com>
2018-03-08 12:21:20 -0500

    deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,8 +4,8 @@ import env from './.env';
 export const environment = {
   production: true,
   version: env.npm_package_version,
-  serverUrl: 'https://github.com/spaceman27/TeslaUI/tree/master/dist',
-  baseUrl: 'https://github.com/spaceman27/TeslaUI/tree/master/dist',
+  serverUrl: 'https://spaceman27.github.io/TeslaUI/dist',
+  baseUrl: 'https://spaceman27.github.io/TeslaUI/dist',
   defaultLanguage: 'en-US',
   supportedLanguages: [
     'en-US',
